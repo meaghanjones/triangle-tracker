c016efb3693aee794c5b4e513f720747bd687e7a
Meaghan Jones and Olivia Hinton <olivia@>
2016-06-02 11:55:16 -0700

    add js styles

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("form#triangle-tracker").submit(function(event) {
+   var side1 = parseInt($("input#side1").val());
+   var side2 = parseInt($("input#side2").val());
+   var side3 = parseInt($("input#side3").val());
+
+
+
+   event.preventDefault();
+  });
